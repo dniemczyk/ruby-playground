9c1953703807a5f1b59d77d1b038799ca9451736
Damian Niemczyk <djniemczyk7@googlemail.com>
2014-10-03 22:36:50 +0200

    Add Ex13 - Parameters

---
+++ hardway/ex13.rb
@@ -0,0 +1,9 @@
+#
+# Trying some unpacking, parameters and variables
+#
+
+first, second, third = ARGV
+
+puts "Your first  variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third  variable is: #{third}"

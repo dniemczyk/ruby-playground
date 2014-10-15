f0a79a531c4ca6653e1c92c104d78b1d8b7947f2
Damian Niemczyk <djniemczyk7@googlemail.com>
2014-10-15 15:33:55 +0200

    Define the report title output

--- patterns/lib/template/report.rb
+++ patterns/lib/template/report.rb
@@ -1,2 +1,15 @@
 class Report
+  def initialize(output)
+    @output = output
+    @title = 'Monthly report'
+    @text  = [ 'Things are going', 'realy, realy well.' ]
+  end
+
+  def output_report
+    @output.puts '<html>'
+    @output.puts '  <head>'
+    @output.puts "    <title> #{@title} </title>"
+    @output.puts '  </head>'
+    @output.puts '</html>'
+  end
 end

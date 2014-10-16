421e9fa18750290fe43039bea6490afa944ee1c4
Damian Niemczyk <djniemczyk7@googlemail.com>
2014-10-16 10:03:06 +0200

    A namespace is overkill for now

--- patterns/spec/template/report_spec.rb
+++ patterns/spec/template/report_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
-module Template
-  describe Report do
-    let(:report) { Report.new }
-    let(:output) { report.output_report }
+describe Report do
+  let(:report) { Report.new }
+  let(:output) { report.output_report }
 
-    it "prints a title" do
-      expect { output.to output(/<title>/).to_stdout }
-    end
+  it "prints a title" do
+    expect { output.to output(/<title>/).to_stdout }
+  end
 
-    it "prints some content" do
-      expect { output.to output(/<p>/).to_stdout }
-    end
+  it "prints some content" do
+    expect { output.to output(/<p>/).to_stdout }
   end
 end
